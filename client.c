4bf157580daf933444492cfeded4b589ec2b314a
KezziaCarter <33240092+KezziaCarter@users.noreply.github.com>
2018-03-09 08:02:47 -0500

    added the ability for server and client to take multiple arguments. now with comments

--- client.c
+++ client.c
@@ -8,45 +8,50 @@
 #include <unistd.h>
 #include <errno.h>
 #include <arpa/inet.h>
+#include "practice_project.h"
 
-int main(int argc, char *argv[])
-{
+/* <client> <server IP> <server port> <file path> <to format> <to name> */
+int main(int argc, char *argv[]) {
     int sockfd = 0, n = 0;
     char recvBuff[1024];
     struct sockaddr_in serv_addr;
 
-    if(argc != 2)
-    {
-        printf("\n Usage: %s <ip of server> \n",argv[0]);
+    /* Ensuring that we receive the correct # of arguments from the user */
+    if (argc != 6) {
+        printf("\n Looking for a command of format: %s <server IP> <server port> <file path> <to format> <to name>\n", argv[0]);
         return 1;
     }
+    int port_num = atoi(argv[2]);
 
     memset(recvBuff, '0',sizeof(recvBuff));
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
+
+    /* creating another socket and descriptor that uses IPv4 and TCP */
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Error : Could not create socket \n");
         return 1;
     }
 
     memset(&serv_addr, '0', sizeof(serv_addr));
 
+    /* IP address of the remote host */
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(5000);
+    /* Port of remote host */
+    serv_addr.sin_port = htons(port_num);
 
-    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
-    {
+    if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0) {
         printf("\n inet_pton error occured\n");
         return 1;
     }
-
-    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
+    /* try to connect this socket with the socket of the remote host. */
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
        printf("\n Error : Connect Failed \n");
        return 1;
     }
 
-    while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
-    {
+    /* server sends message to client's socket through clients socket descriptor
+    and client can read it through normal read call on the its socket
+    descriptor. */
+    while ((n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0) {
         recvBuff[n] = 0;
         if(fputs(recvBuff, stdout) == EOF)
         {
@@ -54,8 +59,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    if(n < 0)
-    {
+    if (n < 0) {
         printf("\n Read error \n");
     }
 
--- server.c
+++ server.c
@@ -8,37 +8,57 @@
 #include <string.h>
 #include <sys/types.h>
 #include <time.h>
+#include "practice_project.h"
 
+/* <server> <port> */
 int main(int argc, char *argv[])
 {
-    printf("I'm aliiiive!!\n");
+    if (argc != 2) {
+      printf("Looking for a command of format: %s <port>\n", argv[0]);
+      return 1;
+    }
+
+    int port_num = atoi(argv[1]);
+    printf("Starting server on port number %i\n", port_num);
     int listenfd = 0, connfd = 0;
     struct sockaddr_in serv_addr;
 
     char sendBuff[1025];
-    time_t ticks;
 
+    /* creating socket and socket descriptor that uses IPv4 address and TCP*/
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     memset(&serv_addr, '0', sizeof(serv_addr));
     memset(sendBuff, '0', sizeof(sendBuff));
 
+    /* family/domain to be used by listening socket */
     serv_addr.sin_family = AF_INET;
+    /* interface the socket will listen on */
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(5000);
+    /* port on which server will wait for clients */
+    serv_addr.sin_port = htons(port_num);
 
+    /* assigns the details specified in the struct ‘serv_addr’ to the socket
+     listenfd. */
     bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 
-    listen(listenfd, 10);
+    /* Up to 5 clients are allowed queue for the listening socket*/
+    listen(listenfd, 5);
 
-    while(1)
-    {
+    /* We need an infinite loop so that the server is always running */
+    while (1) {
+        /* the server is put to sleep and waits for a client request. When the
+        three way TCP handshake* is complete, the socket descriptor representing
+        the client socket is returned. */
         connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
 
-        ticks = time(NULL);
-        snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
+        /* As soon as server gets a request from client, it prepares the stuff
+        and writes it to the client socket through its description */
+        snprintf(sendBuff, sizeof(sendBuff), "Don't delete my brackets thanks");
         write(connfd, sendBuff, strlen(sendBuff));
 
         close(connfd);
+        /* this apparently ensures that server does not eat up all of your CPU
+        processing. [Waka Flocka voice] OK. */
         sleep(1);
      }
 }
